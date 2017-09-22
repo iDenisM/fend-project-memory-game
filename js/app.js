e69ca4c7acc41b694e6b2932aabb7c883b493e3c
iDenis <denis.mitiliuc@gmail.com>
2017-09-23 00:10:32 +0200

    Add mouse click event

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ $(function() {
     }
 
     return array;
-  }
+  };
   /*
   * Display the cards on the page
   *   - shuffle the list of cards using the provided "shuffle" method below
@@ -38,17 +38,30 @@ $(function() {
   *   - add each card's HTML to the page
   */
   shuffle(cards);
-  console.log(cards);
 
+  // Create the HTML block for the card
+  let createCardHTML = (card) => {
+    let content = '';
+    // Loop each card to create the HTML block
+    for (card of cards) {
+      content += '<li class="card"><i class="fa fa-' + card + '"></i></li>';
+    };
+    $('.deck').append(content);
+  };
+
+  createCardHTML(cards);
 
   /*
   * set up the event listener for a card. If a card is clicked:
   *  - display the card's symbol (put this functionality in another function that you call from this one)
-  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
   *  - if the list already has another card, check to see if the two cards match
   *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
   *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
   *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
   *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
   */
+  $('li.card').click(function() {
+    $(this).toggleClass('open show');
+  });
 });
